7a41373869684bb5b83c3636a00d01d242c7bc2a
Osaro Iyoha <saro.iyoha@gmail.com>
2023-06-29 08:57:43 +0100

    Subscribe Section: Initial Styling

--- components/Subscribe.jsx
+++ components/Subscribe.jsx
@@ -1,16 +1,22 @@
 import style from "@/components/Subscribe.module.css"
+import Image from "next/image"
+import emailMktImg from "@/assets/email_marketing.png"
 function Subscribe() {
   return (
     <section className={`section ${style.section__subscribe}`}>
       <div className={`section__inner ${style.section__inner}`}>
-      <div className={`${style.media}`}></div>
+      <div className={`${style.media}`}>
+        <Image 
+        src={emailMktImg}
+        />
+      </div>
         <div className="content">
             <h3>Subscribe to our newsletter to get exclusive updates from Us</h3>
             <p>Receive latest news, update, and many other things every week</p>
-            <div>
+            <form>
                 <input type="text" />
-                <button>Subscribe</button>
-            </div>
+                <button className="btn btn-primary">Subscribe</button>
+            </form>
         </div>
       </div>
         
