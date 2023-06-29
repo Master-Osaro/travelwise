f60fb4a7ea53325ed775197d1a2377ee3098ddef
Osaro Iyoha <saro.iyoha@gmail.com>
2023-06-29 07:34:43 +0100

    Add location Images

--- components/Locations.jsx
+++ components/Locations.jsx
@@ -1,38 +1,52 @@
 import style from "@/components/Locations.module.css"
+import Image from "next/image"
+import location1 from "@/assets/locations/location1.jpg"
+import location2 from "@/assets/locations/location2.jpg"
+import location3 from "@/assets/locations/location3.jpg"
+import location4 from "@/assets/locations/location4.jpg"
+import location5 from "@/assets/locations/location5.jpg"
 function Locations() {
   return (
     <section className={`section ${style.section__locations} bg__grey`}>
-        <div className="section__inner">
-            <h2>Population Locations</h2>
-            <p>There are many packages more than you need just to make sure that you get enough options to avail the best one</p>
+        <div className={`section__inner ${style.section__inner}`}>
+            <h2>Popular Locations</h2>
+            <p><span>There are many packages more than you need just to make sure that you get enough options to avail the best one</span></p>
             <div className={`${style.location__list} location__list`}>
                 <div className={`${style.location}`}>
-                    <img src="" alt="" />
-                    <h3>Mountain Hiking Tour</h3> <div>4.5</div>
+                    <Image src={location1} alt="" />
+                    <div className={style.location__header}><h3>Mountain Hiking Tour</h3> <div className={style.rating}>4.5</div></div>
                     <p>Himalayas, Hawaii</p>
-                    <p><span>$1000</span>/person</p>
-                    <button className="btn btn-primary">Book now</button>
+                    <div className={style.book_btn__group}>
+                        <p><span>$1000</span>/person</p>
+                        <button className="btn btn-primary">Book now</button>
+                    </div>
                 </div>
                 <div className={`${style.location}`}>
-                    <img src="" alt="" />
-                    <h3>Mountain Hiking Tour</h3> <div>4.5</div>
+                    <Image src={location2} alt="" />
+                    <div className={style.location__header}><h3>Mountain Hiking Tour</h3> <div className={style.rating}>4.5</div></div>
                     <p>Himalayas, Hawaii</p>
-                    <p><span>$1000</span>/person</p>
-                    <button className="btn btn-primary">Book now</button>
+                    <div className={style.book_btn__group}>
+                        <p><span>$1000</span>/person</p>
+                        <button className="btn btn-primary">Book now</button>
+                    </div>
                 </div>
                 <div className={`${style.location}`}>
-                    <img src="" alt="" />
-                    <h3>Mountain Hiking Tour</h3> <div>4.5</div>
+                    <Image src={location3} alt="" />
+                    <div className={style.location__header}><h3>Mountain Hiking Tour</h3> <div className={style.rating}>4.5</div></div>
                     <p>Himalayas, Hawaii</p>
-                    <p><span>$1000</span>/person</p>
-                    <button className="btn btn-primary">Book now</button>
+                    <div className={style.book_btn__group}>
+                        <p><span>$1000</span>/person</p>
+                        <button className="btn btn-primary">Book now</button>
+                    </div>
                 </div>
                 <div className={`${style.location}`}>
-                    <img src="" alt="" />
-                    <h3>Mountain Hiking Tour</h3> <div>4.5</div>
+                    <Image src={location4} alt="" />
+                    <div className={style.location__header}><h3>Mountain Hiking Tour</h3> <div className={style.rating}>4.5</div></div>
                     <p>Himalayas, Hawaii</p>
-                    <p><span>$1000</span>/person</p>
-                    <button className="btn btn-primary">Book now</button>
+                    <div className={style.book_btn__group}>
+                        <p><span>$1000</span>/person</p>
+                        <button className="btn btn-primary">Book now</button>
+                    </div>
                 </div>
             </div>
         </div>
